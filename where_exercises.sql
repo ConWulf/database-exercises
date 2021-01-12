7c5725c629452d751db5482a4cdf5c294ad1a20d
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-12 15:31:42 -0600

    where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,8 +4,17 @@ describe employees;
 
 select *
 from employees
-where first_name
-in ('Irena', 'Vidya', 'Maya');
+where gender like 'M' and (
+        first_name like 'Irena'
+        or first_name like 'Vidya'
+        or first_name like 'Maya'
+    );
+
+select *
+from employees
+where first_name like 'Irena'
+        or first_name like 'Vidya'
+        or first_name like 'Maya';
 
 select first_name, last_name as 'last name start with e'
 from employees
@@ -13,7 +22,17 @@ where last_name like 'E%';
 
 select *
 from employees
-where hire_date like '199%';
+where last_name like '%E'
+    or last_name like 'E%';
+
+select *
+from employees
+where last_name like '%E'
+   and last_name like 'E%';
+
+select *
+from employees
+where hire_date between cast('1990-01-01' as date) and cast('1999-12-31' as date);
 
 select *
 from employees
@@ -22,3 +41,4 @@ where birth_date like '%12-25';
 select *
 from employees
 where last_name like '%q%';
+
